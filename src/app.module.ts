b72cb793cd788724fbf6856e5f65ebd0d17108a5
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-03-14 03:09:46 +0600

    new module connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { SocietyModule } from './society/society.module';
+import { SocietiesModule } from './societies/societies.module';
+import { PredefinedServiceChargesModule } from './predefined-service-charges/predefined-service-charges.module';
+import { ServiceChargesModule } from './service-charges/service-charges.module';
+import { UserServiceChargesModule } from './user-service-charges/user-service-charges.module';
+import { FlatsModule } from './flats/flats.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ConfigModule } from '@nestjs/config';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, SocietyModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule, 
+    UsersModule, 
+    SocietiesModule, 
+    PredefinedServiceChargesModule, 
+    ServiceChargesModule, 
+    UserServiceChargesModule,
+    FlatsModule, 
+    PaymentsModule, RolesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
