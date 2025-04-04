5ced5d27d36542c4d9a85400c9da7a6ae0f575fc
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-04-04 13:34:21 +0600

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // <-- your frontend domain ['http://localhost:8080']
+    credentials: true,                // If you're using cookies or auth headers
+  });
   const allExceptionsFilter = app.get(AllExceptionsFilter);
   app.useGlobalFilters(allExceptionsFilter);
   app.useGlobalPipes(new ValidationPipe());
