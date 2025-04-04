a9cc25d7d4a9bc37468faef63f620eebe9f3ed9c
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-04-04 14:02:06 +0600

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,14 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-      next();
-  });
 
   app.enableCors({
-      allowedHeaders:"*",
-      origin: "*"
+    origin: true, // Reflects the request origin (e.g., http://localhost:8080) in the response
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Explicitly allow all methods including OPTIONS
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Specify allowed headers
+    credentials: true, // If you need cookies or auth headers
   });
+  
   const allExceptionsFilter = app.get(AllExceptionsFilter);
   app.useGlobalFilters(allExceptionsFilter);
   app.useGlobalPipes(new ValidationPipe());
