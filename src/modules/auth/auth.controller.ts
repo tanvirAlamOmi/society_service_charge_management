9e1c8b1cb8af66bd67721aea624cf787e4a6c8fb
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-04-05 21:58:11 +0600

    option handle

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 // src/auth/auth.controller.ts
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, Options } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Public } from './decorators/public.decorator';
@@ -7,7 +7,11 @@ import { Public } from './decorators/public.decorator';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  
+  @Options('login')
+  handleOptions() {
+    return; // The cors middleware will add the necessary headers
+  }
+
   @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
