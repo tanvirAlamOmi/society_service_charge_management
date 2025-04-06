34f1fdde6232916258dd5c5aa2646d00165491d2
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-04-06 19:05:23 +0600

    cors

--- src/modules/auth/jwt/jwt-auth.guard.ts
+++ src/modules/auth/jwt/jwt-auth.guard.ts
@@ -9,6 +9,11 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       }
     
       canActivate(context: ExecutionContext) {
+
+        const request = context.switchToHttp().getRequest();
+        if (request.method === 'OPTIONS') {
+          return true; // Skip authentication for OPTIONS requests
+        }
         // Check if the route is marked as public
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
           context.getHandler(),
